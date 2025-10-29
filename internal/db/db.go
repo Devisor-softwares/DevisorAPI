db92d6ca13ba8469a1c89a32ed9bd4c908d3b13a
allexanderxha <allexanderxha@icloud.com>
2025-10-29 15:21:24 +0100

    Some more information regrading the database handler.

--- internal/db/db.go
+++ internal/db/db.go
@@ -12,6 +12,11 @@ func ConnectDB(dsn string) (*gorm.DB, error) {
         return nil, err
     }
 
+    // Here, we essentially migrate our models.
+    // We take the models.User, which simply get's
+    // the user model from the modules module.
+    // We do the same for server, which are both
+    // not implemented yet.
     db.AutoMigrate(&models.User{}, &models.Server{})
 
     return db, nil
