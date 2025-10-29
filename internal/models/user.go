d0c075fa70dc71f9223ef2595ef035f93931abab
allexanderxha <allexanderxha@icloud.com>
2025-10-29 15:40:08 +0100

    Added the helperFunctions within the models/user.go

--- internal/models/user.go
+++ internal/models/user.go
@@ -32,7 +32,7 @@ type User struct {
 	APIKeys   []APIKey     `gorm:"foreignKey:UserID"`
 }
 
-// TODO: Create the HelperFunctions, which do the following:
+// The HelperFunctions do the following:
 // - BeforeCreate hook to generate UUID
 // - SetPassword hashes and sets the user's password
 // - CheckPassword validates a password against the stored hash
@@ -41,3 +41,37 @@ type User struct {
 // - AccessibleServers can preload all servers the user owns
 
 // NOTE: This is taken as reference from Pterodactyl.
+
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	u.UUID = uuid.New().String()
+	return
+}
+
+func (u *User) SetPassword(password string) error {
+	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.PasswordHash = string(hash)
+	return nil
+}
+
+
+func (u *User) CheckPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.PasswordHash), []byte(password))
+	return err == nil
+}
+
+func (u *User) FullName() string {
+	return u.FirstName + " " + u.LastName
+}
+
+func (u *User) IsAdmin() bool {
+	return u.RootAdmin
+}
+
+func (u *User) AccessibleServers(db *gorm.DB) ([]Server, error) {
+	var servers []Server
+	err := db.Preload("Owner").Where("owner_id = ?", u.ID).Find(&servers).Error
+	return servers, err
+}
